fe212b6f066f762c75d3ba50b1964ea024444f63
Kkun84 <kesuike84@gmail.com>
2021-04-09 20:15:06 +0900

    Add GPU setting

--- train/train.py
+++ train/train.py
@@ -1,6 +1,6 @@
 """
 Usage:
-    train.py [--max_epochs=<int>] [--batch_size=<int>] [--image_size=<int>] [--lr=<float>]
+    train.py [--max_epochs=<int>] [--batch_size=<int>] [--image_size=<int>] [--lr=<float>] [-g=<int>...]
 
 Options:
     -h --help           Show this screen.
@@ -8,13 +8,12 @@ Options:
     --batch_size=<int>  Batch size [default: 16].
     --image_size=<int>  Image width & height [default: 128].
     --lr=<float>        Learning rate.
+    -g, --gpus=<int>    Use GPU [default: 0].
 """
 
-from docopt import docopt
-
 import pytorch_lightning as pl
-
-from lightning_module import DataModule, GAN
+from docopt import docopt
+from lightning_module import GAN, DataModule
 
 
 def main():
@@ -25,14 +24,14 @@ def main():
     max_epochs = int(args['--max_epochs'])
     batch_size = int(args['--batch_size'])
     image_size = int(args['--image_size'])
-
     lr = args['--lr'] and float(args['--lr'])
+    gpus = [int(i) for i in args['--gpus']]
 
     datamodule = DataModule(batch_size=batch_size, image_size=image_size)
     model = GAN(**{k: v for k, v in dict(lr=lr).items() if v is not None})
 
     trainer = pl.Trainer(
-        gpus=1,
+        gpus=gpus,
         max_epochs=max_epochs,
         progress_bar_refresh_rate=1,
     )
